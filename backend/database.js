96165b2e56c19c302c248f4a5d39254e19f7be1b
guipmuller <guilhermepmuller@gmail.com>
2024-11-04 21:10:23 -0300

    Chore: improvements in the code to fix render database

---
+++ backend/database.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+  logging: console.log,
+});
+
+module.exports = sequelize;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,5 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: console.log,
-});
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
 
 const User = sequelize.define(
   "User",
